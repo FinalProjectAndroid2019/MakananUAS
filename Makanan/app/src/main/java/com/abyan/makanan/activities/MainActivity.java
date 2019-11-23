7cd70d3f404bba9cf1fc130eea1e33dbda859362
Abyan <harlem.minion09@gmail.com>
2019-11-23 07:15:26 +0700

    add navigation

--- Makanan/app/src/main/java/com/abyan/makanan/activities/MainActivity.java
+++ Makanan/app/src/main/java/com/abyan/makanan/activities/MainActivity.java
@@ -1,16 +1,60 @@
 package com.abyan.makanan.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.abyan.makanan.R;
+import com.abyan.makanan.fragments.DrinkFragment;
+import com.abyan.makanan.fragments.FoodFragment;
+import com.abyan.makanan.fragments.PesanFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new FoodFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new FoodFragment();
+                break;
+            case R.id.action_drink:
+                fragment = new DrinkFragment();
+                break;
+            case R.id.action_pesan:
+                fragment = new PesanFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
